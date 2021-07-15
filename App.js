8eeba66387a7b733d491623b4b8312024b243617
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-15 23:51:37 +0530

    import the Calculator component inside the App.js file

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants';
+import Calculator from './components/Calculator';
 
 export default function App() {
   return (
