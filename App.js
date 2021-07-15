8a275d324844dd683f300a122aac88b48878e45d
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-15 23:45:28 +0530

    remove the Text tag from the View tag

--- App.js
+++ App.js
@@ -6,7 +6,6 @@ import Constants from 'expo-constants';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
