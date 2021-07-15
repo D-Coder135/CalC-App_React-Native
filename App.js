b2c2af82dcf79a424e12916bdd0dc88cbb1eca68
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-15 23:53:53 +0530

    add the Calculator tag

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
+      <Calculator />
     </View>
   );
 }
--- components/Calculator.js
+++ components/Calculator.js
@@ -10,7 +10,7 @@ class Calculator extends Component {
 }
 
 const styles = StyleSheet.create({
-     
+       
 });
 
 
