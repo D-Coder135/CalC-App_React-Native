5ab9bee856fe78ca53f707a62e5969dd95d6c422
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-16 00:01:40 +0530

    add some code inside the Buttons.js

---
+++ components/Buttons.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {View, StyleSheet} from 'react-native';
+
+class Buttons extends Component {
+    render () {
+        return(
+            <View style = {styles.buttonsComponent}>
+                Calculator
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    buttonsComponent: {
+        flex: 1,
+        borderWidth: 1,
+        borderColor: 'skyblue'
+    }
+});
+
+
+export default Buttons;
