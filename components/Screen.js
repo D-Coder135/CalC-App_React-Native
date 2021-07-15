d2643576e3ecec6892ca23b48e5c7ab75d64f718
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-16 00:00:42 +0530

    add some code inside the Screen.js

---
+++ components/Screen.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {View, StyleSheet} from 'react-native';
+
+class Screen extends Component {
+    render () {
+        return(
+            <View style = {styles.screenComponent}>
+                Calculator
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    screenComponent: {
+        flex: 1,
+        borderWidth: 1,
+        borderColor: 'skyblue'
+    }
+});
+
+
+export default Screen;
