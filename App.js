62bf1957e2fa308e08206deaea3cab98032ecbd2
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-15 23:42:54 +0530

    import the Constants component from the expo-constants library

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
 
 export default function App() {
   return (
