054afc5431fea10faec023711de0e4cab5db74a0
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-16 00:01:07 +0530

    update the text inside the View tag

--- components/Screen.js
+++ components/Screen.js
@@ -5,7 +5,7 @@ class Screen extends Component {
     render () {
         return(
             <View style = {styles.screenComponent}>
-                Calculator
+                Screen
             </View>
         );
     }
