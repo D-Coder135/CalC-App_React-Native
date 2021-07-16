6f30d5ba32e34b278be1f07e55f0d1298c047086
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-16 13:57:14 +0530

    add a StatusBar tag

--- components/Calculator.js
+++ components/Calculator.js
@@ -7,6 +7,7 @@ class Calculator extends Component {
     render () {
         return(
             <View style = {styles.calculatorComponent}>
+                <StatusBar />
                 <Screen />
                 <Buttons />
             </View>
