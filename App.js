e09d13c68c0ad0a3d2470cbfa3f6cb29056b8e12
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-16 13:01:47 +0530

    comment out some part of the code

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants';
@@ -7,7 +6,6 @@ import Calculator from './components/Calculator';
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
       <Calculator />
     </View>
   );
@@ -17,7 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     // backgroundColor: 'skyblue',
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
     // padding: 8,
