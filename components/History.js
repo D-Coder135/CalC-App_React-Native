014b1bebfbfb41f2a8efcb72cb32cdc55bf35efc
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-18 23:54:12 +0530

    add the borderBottomLeftRadius property

--- components/History.js
+++ components/History.js
@@ -29,7 +29,9 @@ const styles = StyleSheet.create({
 
     historyContainer: {
         flex: 8,
-        backgroundColor: 'skyblue'
+        backgroundColor: 'skyblue',
+        borderBottomLeftRadius: 20,
+        
     },
 
     disabledArea: {
