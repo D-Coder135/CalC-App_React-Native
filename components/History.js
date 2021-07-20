95c59c43f6ba3f57db07eefb6674770839389ff1
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-20 09:42:40 +0530

    comment out some part of the code

--- components/History.js
+++ components/History.js
@@ -103,7 +103,7 @@ const styles = StyleSheet.create({
     headerClearButton: {
         textAlign: 'right',
         fontSize: 30,
-        borderWidth: 1,
+        // borderWidth: 1,
     },
 
     clearButton: {
